d3c3949c3b11dd7c5871e381bc243eab518ebb28
johnko <279736+johnko@users.noreply.github.com>
2025-11-08 00:13:54 -0500

    shellcheck

--- .github/helm-dep.sh
+++ .github/helm-dep.sh
@@ -21,14 +21,14 @@ if git diff --name-only | grep lab/renovatebotwrapper; then
 
   # clean chart sources since files could have been deleted upstream
   for i in $(ls -1 *.tgz | xargs -t -I{} -P1 tar tf {} 2>/dev/null | cut -f1 -d/ | sort -u); do
-    if [ -d ./$i/ ]; then
-      rm -fr ./$i/
+    if [ -d ./"$i"/ ]; then
+      rm -fr ./"$i"/
     fi
   done
 
   # extract chart sources
-  ls -1 *.tgz | xargs -t -I{} -P1 tar xvf {}
+  ls -1 ./*.tgz | xargs --max-procs=1 --verbose -I{} tar xvf {}
 
-  rm -v *.tgz
+  rm -v ./*.tgz
 
 fi
