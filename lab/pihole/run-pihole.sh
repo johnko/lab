a2cec8fd39ca023b81bb5b726d4775d9b5e2d02d
johnko <279736+johnko@users.noreply.github.com>
2025-11-08 00:23:01 -0500

    shellcheck

--- lab/pihole/run-pihole.sh
+++ lab/pihole/run-pihole.sh
@@ -4,7 +4,7 @@ set -eux
 case $1 in
   web | *)
     set +x
-    echo Password: $(kubectl -n pihole get secret pihole-password -o jsonpath="{.data.password}" | base64 -d)
+    echo Password: "$(kubectl -n pihole get secret pihole-password -o jsonpath='{.data.password}' | base64 -d)"
     echo
     set -x
     kubectl port-forward service/pihole-web -n pihole 8081:80 &
