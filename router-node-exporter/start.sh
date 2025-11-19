f7495e27fd87e9e1c58505283e9803c0e0aadb02
johnko <279736+johnko@users.noreply.github.com>
2025-11-19 17:28:11 -0500

    use --no-collector.netdev.netlink

--- router-node-exporter/start.sh
+++ router-node-exporter/start.sh
@@ -3,4 +3,9 @@ set -exuo pipefail
 
 /tmp/home/root/bin/node_exporter \
   --no-collector.filesystem \
+  --no-collector.netdev.netlink \
   --web.disable-exporter-metrics
+
+# --no-collector.filesystem because it's complaining and no realy disk to monitor in router anyway
+# --no-collector.netdev.netlink to fall back to /proc/net/dev https://github.com/prometheus/node_exporter/issues/2502
+# --web.disable-exporter-metrics to not monitor itself
